561210509efada92a6eec3faa42e5e1e300be427
Martin DeMello <mdemello@google.com>
2022-03-11 17:54:13 -0800

    add windicss

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,5 +1,6 @@
 import { Application } from 'stimulus'
 import { registerControllers } from 'stimulus-vite-helpers'
+import 'windi.css'
 
 const application = Application.start()
 const controllers = import.meta.globEager('./**/*_controller.js')
--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,19 @@ import { defineConfig } from 'vite'
 import RubyPlugin from 'vite-plugin-ruby'
 import FullReload from 'vite-plugin-full-reload'
 import StimulusHMR from 'vite-plugin-stimulus-hmr'
+import WindiCSS from 'vite-plugin-windicss'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
     FullReload(['config/routes.rb', 'app/views/**/*']),
     StimulusHMR(),
+    WindiCSS({
+      root: __dirname,
+      scan: {
+        fileExtensions: ['erb', 'haml', 'html', 'vue', 'js', 'ts', 'jsx', 'tsx'],
+        dirs: ['app/views', 'app/frontend'],
+      },
+    }),
   ],
 })
