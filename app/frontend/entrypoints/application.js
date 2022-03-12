32e1e5150ff893e96ffbb9f84d984f8b080c0a4d
Martin DeMello <mdemello@google.com>
2022-03-11 17:45:30 -0800

    add stimulus, vite helpers and hmr plugin

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,28 +1,8 @@
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
+import { Application } from 'stimulus'
+import { registerControllers } from 'stimulus-vite-helpers'
+
+const application = Application.start()
+const controllers = import.meta.globEager('./**/*_controller.js')
+registerControllers(application, controllers)
+
 console.log('Vite ⚡️ Rails')
-
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
-
-console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify.app/guide/rails')
-
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
