ce2808169999f0114130857c3ecedd63bbe1fe1d
Martin DeMello <mdemello@google.com>
2022-03-21 21:09:16 -0700

    fix up results migration

--- db/migrate/20220322040454_create_results.rb
+++ db/migrate/20220322040454_create_results.rb
@@ -2,15 +2,16 @@ class CreateResults < ActiveRecord::Migration[7.0]
   def change
     create_table :results do |t|
       t.belongs_to :tournament, null: false, foreign_key: true
+      t.belongs_to :entrant, null: false, foreign_key: true
       t.string :division
       t.string :round
-      t.references :entrant, null: false, foreign_key: true
-      t.references :opponent, null: false, foreign_key: true
+      t.references :opponent, null: false
       t.integer :score
       t.integer :opponent_score
       t.boolean :first
 
       t.timestamps
     end
+    add_foreign_key :results, :entrants, column: :opponent_id
   end
 end
